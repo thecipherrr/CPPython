ac019b962481031c87397e550217c744afe7ec00
thecipherrr <edwardmk2704@gmail.com>
2022-05-29 10:34:47 +0700

    added print, and header translation

--- translate.py
+++ translate.py
@@ -1,35 +1,54 @@
 import lexer as lex
 import py_parser as yacc
+import os
 
 class Translate:
-    def __init__(self, ast_input, file_output):
+    def __init__(self, ast_input, file_input):
         self.ast_input = ast_input
-        self.file_output = file_output
-    
+        self.file_input = file_input
+
+    def write_header(self, out):
+        out.write("#include<iostream>")
+        out.write("\n\n")
+        out.write("using namespace std;")
+        out.write("\n\n")
+
+    def write_print(self, out, print_args):
+        out.write("cout << " + print_args + "<< endl;")
+         
+
     def compile(self):
-        with open(self.file_output, "w") as out:
-            out.write("#include<iostream>")
-            out.write("\n\n")
-            out.write("using namespace std;")
-            out.write("\n\n")
-        
-            for syntax in self.ast_input:
+        filename = os.path.splitext(self.file_input)[0]
+        filename_out = filename + ".cpp"
+        out = open(filename_out, "w+")
+       
+        self.write_header(out)
+        for syntax in self.ast_input:
+            # if function declaration, write outside of int main(void)
+            if isinstance(syntax, yacc.FunctionDeclaration):
+                # TODO: implement translation for function declaratin later
+                out.write("foo")
+
+            else:
                 if isinstance(syntax, yacc.PythonFunction):
-                    if syntax.t_identifier.t_value == "print":
-                        print(syntax.t_args.token.t_value)
-                        out.write("int main(void)")
-                        out.write("\n")
-                        out.write("{")
-                        out.write("\n")
-                        out.write("\t cout << " + syntax.t_args.token.t_value + " << endl;")
-                        out.write("\n \t return 0;")
-                        out.write("\n}")
-                        print("Compilation succesful")
-                        return 0
-                if isinstance(syntax, yacc.VariableCreation):
-                    if syntax.t_identifier.t_value == ""
-                
-                
+                   if syntax.t_identifier.t_value == "print":
+                        out.write("\t")
+                        self.write_print(out, syntax.t_args.token.t_value) 
+                        
+                elif isinstance(syntax, yacc.VariableCreation):
+                    if syntax.t_identifier.t_value == "":
+                        # TODO: implement variable creation later
+                        print("foo")
+               
+                elif syntax==None:
+                    print("Compilation Successful!")
+                    return 0;
+
+                out.write("\n")
+                out.write("\t return 0;")
+                out.write("\n}")
+        
+        out.close()
 
 def main():
     with open("test.py") as data:
@@ -41,7 +60,7 @@ def main():
     parser = yacc.Parser("test.py", tokens, program)
     ast = parser.parse()
 
-    trans = Translate(ast, "test.cpp")
+    trans = Translate(ast, "test.py")
     out = trans.compile()
 
 
