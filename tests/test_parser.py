7c30ed3742066e082900f4ec2bd7d58b3ffe55f7
Leon <doncorleone142857@gmail.com>
2022-06-11 15:09:33 +0700

    changed test_parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,1 +1,16 @@
-print("hello", "world", 7, 9)
+def hello(a, b, c):
+    print("hello world")
+
+print()
+a = 100
+b = "what" + "is" + "this"
+print("hehe" + "boi", 9)
+if "hello":
+    a = 9 + 7
+    print("hello", "world")
+elif 19 + 7:
+    b = 10 * 100
+    print(9, 7)
+else:
+    c = "what" + "the" + "hell"
+    print("yolo")
